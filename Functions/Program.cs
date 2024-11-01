2ff0dbfd597c362450e4b502638181cda26ced9e
Marten. <marten.aun@tkt.ee>
2024-11-01 15:57:41 +0200

    add functions code

---
+++ Functions/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Are u coming or leaving?");
+string userChoice = Console.ReadLine();
+if (userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();  
+}
+
+
+PrintHello();
+PrintGoodBye();
+static void PrintHello()
+{
+    Console.WriteLine("Hello, World!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, aligator");   
+}
